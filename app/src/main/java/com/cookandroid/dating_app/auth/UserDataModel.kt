0dbed465fddc7a78681d0c241fec8b8457c83813
esak <lsls4868@gmail.com>
2022-06-13 00:00:46 +0900

    dating_app like

---
+++ app/src/main/java/com/cookandroid/dating_app/auth/UserDataModel.kt
@@ -0,0 +1,9 @@
+package com.cookandroid.dating_app.auth
+
+class UserDataModel (
+    val uid: String? = null,
+    val age: String? = null,
+    val nickname: String? = null,
+    val gender: String? = null,
+    val city: String? = null
+)
